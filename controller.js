caad6e533317f31919b7cbe325be83978a72a175
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-02-04 15:24:21 +0100

    Ferdig bildemodus

--- controller.js
+++ controller.js
@@ -10,3 +10,213 @@ if(thing != null){
     console.log("click");
 }
 }
+
+function chooseCut(cut, index){
+    const rib = model.cuts[0];
+    const chuck = model.cuts[1];
+    const brisket = model.cuts[2];
+    const plate = model.cuts[3];
+    const sirloin = model.cuts[4];
+    const topSirloin = model.cuts[5];
+    const tenderloin = model.cuts[6];
+    const flank = model.cuts[7];
+    const bottomSirloin = model.cuts[8];
+    const rounds = model.cuts[9];
+    const shank = model.cuts[10];
+    const shortLoin = model.cuts[11];
+    switch(cut){
+        case "chuck":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "rib":
+            chuck.chosen = "not-chosen";
+            rib.chosen = "chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "brisket":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "plate":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "sirloin":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "topsirloin":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "tenderloin":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "flank":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "bottomsirloin":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "rounds":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "shank":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "chosen";
+            shortLoin.chosen = "not-chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+        case "shortloin":
+            rib.chosen = "not-chosen";
+            chuck.chosen = "not-chosen";
+            brisket.chosen = "not-chosen";
+            plate.chosen = "not-chosen";
+            sirloin.chosen = "not-chosen";
+            topSirloin.chosen = "not-chosen";
+            tenderloin.chosen = "not-chosen";
+            flank.chosen = "not-chosen";
+            bottomSirloin.chosen = "not-chosen";
+            rounds.chosen = "not-chosen";
+            shank.chosen = "not-chosen";
+            shortLoin.chosen = "chosen";
+            picturePgView();
+            picPgInfo(index)
+            break;
+    }
+
+}
--- view.js
+++ view.js
@@ -4,7 +4,7 @@ function buttonPgView() {
                         <div class="button-page">
                             <img src="https://cdn.discordapp.com/attachments/796668478261297152/800658341361614858/ButcherIT_logo_1.png" alt="ButcherIT Logo">
                             <button class="button button5" onclick="picturePgView()">Bilde modus</button>
-                            <button class="button button5">Quiz</button>
+                            <button class="button button5" onclick="quizPgView()">Quiz</button>
                             <button class="button button5" onclick="listPgView()">Liste modus</button>
                         </div>
                       </div>`;
@@ -62,86 +62,111 @@ function picturePgView(){
                             </div>
                             <div id="cow" style="text-align: center"><img src="images/ku2.png" width="1132" height"676" alt="heileKua" usemap="#cowMap">
                                 <map id="cowMap" name="cowMap">
-                                <h2 class="chuck-txt ${model.chosenCut}">chuck</h2>
+                                <h2 class="chuck-txt ${model.cuts[1].chosen}"
+                                    onclick="picPgInfo(1); chooseCut('chuck', 1)">Chuck</h2>
                                 <area class="chuck" 
                                       shape="poly" 
                                       coords="260,55, 253,90, 280,300, 445,300, 440,100 " 
                                       alt="chuck" 
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(1)">
+                                      onclick="picPgInfo(1); chooseCut('chuck', 1)">
+                                <h2 class="rib-txt ${model.cuts[0].chosen}"
+                                    onclick="picPgInfo(0); chooseCut('rib', 0)">Rib</h2>
                                 <area class="rib" 
                                       shape="poly" 
                                       coords="450,100, 460,297, 666,298, 675,185, 655,104, 558,109" 
                                       alt="rib"
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(0)">
+                                      onclick="picPgInfo(0); chooseCut('rib', 0)">
+                                <h2 class="brisket-txt ${model.cuts[2].chosen}"
+                                    onclick="picPgInfo(2); chooseCut('brisket', 2)">Brisket</h2>
                                 <area class="brisket" 
                                       shape="poly" 
                                       coords="295,318, 333,400, 445,403, 450,312" 
                                       alt="brisket"                                      
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(2)">
+                                      onclick="picPgInfo(2); chooseCut('brisket', 2)">
+                                <h2 class="plate-txt ${model.cuts[3].chosen}"
+                                      onclick="picPgInfo(3); chooseCut('plate', 3)">Plate</h2>
                                 <area class="plate" 
                                       shape="poly" 
                                       coords="458,309, 460,403, 660,388, 657,340, 660,312" 
                                       alt="plate"                                        
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(3)">
+                                      onclick="picPgInfo(3); chooseCut('plate', 3)">
+                                <h2 class="shortloin-txt ${model.cuts[11].chosen}"
+                                      onclick="picPgInfo(11); chooseCut('shortloin', 11)">Shortloin</h2>
                                 <area class="shortloin" 
                                       shape="poly" 
                                       coords="670,103, 685,168, 689,231, 675,297, 805,315, 820,254, 815,173, 700,172, 730,152, 805,135, 793,89" 
                                       alt="shortloin"                                       
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(11)">
+                                      onclick="picPgInfo(11); chooseCut('shortloin', 11)">
+                                <h2 class="sirloin-txt ${model.cuts[4].chosen}"
+                                      onclick="picPgInfo(4); chooseCut('sirloin', 4)">Sirloin</h2>
                                 <area class="sirloin" 
                                       shape="poly" 
                                       coords="800,87, 810,134, 900,119, 918,106, 907,73" 
                                       alt="sirloin"                                       
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(4)">
+                                      onclick="picPgInfo(4); chooseCut('sirloin', 4)">
+                                <h2 class="tenderloin-txt ${model.cuts[6].chosen}"
+                                      onclick="picPgInfo(6); chooseCut('tenderloin', 6)">Tenderloin</h2>
                                 <area class="tenderloin" 
                                       shape="poly" 
                                       coords="720,167, 800,146, 881,132, 920,120, 930,150, 830,165" 
                                       alt="tenderloin"                                       
                                       style="cursor: 
                                       pointer" 
-                                      onclick="picPgInfo(6)">
+                                      onclick="picPgInfo(6); chooseCut('tenderloin', 6)">
+                                <h2 class="topsirloin-txt ${model.cuts[5].chosen}"
+                                      onclick="picPgInfo(5); chooseCut('topsirloin', 5)">Top Sirloin</h2>
                                 <area class="topSirloin" 
                                       shape="poly" 
                                       coords="823,173, 825,200, 925,198, 942,203, 933,159" 
                                       alt="topSirloin"                                        
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(5)">
+                                      onclick="picPgInfo(5); chooseCut('topsirloin', 6)">
+                                <h2 class="bottomsirloin-txt ${model.cuts[8].chosen}"
+                                      onclick="picPgInfo(8); chooseCut('bottomsirloin', 8)">Bottom Sirloin</h2>
                                 <area class="bottomSirloin" 
                                       shape="poly" 
                                       coords="826,208, 826,257, 822,288, 813,317, 900,343, 925,355, 937,360, 947,296, 942,214, " 
                                       alt="bottomSirloin"                                      
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(8)">
+                                      onclick="picPgInfo(8); chooseCut('bottomsirloin', 8)">
+                                <h2 class="flank-txt ${model.cuts[7].chosen}"
+                                      onclick="picPgInfo(7); chooseCut('flank', 7)">Flank</h2>
                                 <area class="flank" 
                                       shape="poly" 
                                       coords="672,310, 668,347, 673,386, 832,374, 912,359, 861,336" 
                                       alt="flank"                                       
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(7)">
+                                      onclick="picPgInfo(7); chooseCut('flank', 7)">
+                                <h2 class="rounds-txt ${model.cuts[9].chosen}"
+                                      onclick="picPgInfo(9); chooseCut('rounds', 9)">Rounds</h2>
                                 <area class="round" 
                                       shape="poly" 
                                       coords="916,75, 951,197, 958,280, 954,321, 948,359, 1040,359, 1069,275, 1073,177, 1047,91, 1050,54" 
                                       alt="round" 
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(9)">
+                                      onclick="picPgInfo(9); chooseCut('rounds', 9)">
+                                <h2 class="shankR-txt ${model.cuts[10].chosen}"
+                                      onclick="picPgInfo(2); chooseCut('shank', 10)">Shank</h2>
                                 <area class="shankR" 
                                       shape="poly" 
                                       coords="925,370, 995,446, 1050,450, 1045,367" 
                                       alt="shankR"                                    
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(10)">
+                                      onclick="picPgInfo(10); chooseCut('shank', 10)">
+                                <h2 class="shankF-txt ${model.cuts[10].chosen}"
+                                      onclick="picPgInfo(10); chooseCut('shank', 10)">Shank</h2>
                                 <area class="shankF" 
                                       shape="poly" 
                                       coords="369,412, 395,488, 437,488, 455,414" 
                                       alt="shankF"  
                                       style="cursor: pointer" 
-                                      onclick="picPgInfo(10)">
+                                      onclick="picPgInfo(10); chooseCut('shank', 10)">
                                 </map>
                             </div>
                             <div id="pic-info">
@@ -160,4 +185,13 @@ function picPgInfo(index){
     <h2>Bruksområder:<p id="cut-uses">${cut.use}</p></h2>
     `;
     picInfoDiv.innerHTML = pictureInfo;
+}
+
+function quizPgView() {
+    let quizPage = `
+                    <button id="returnbtn" class="fa fa-home" onclick="buttonPgView()"></button>
+                    <h1>QUIZ MODE</h1>
+                
+    `;
+    appHTML.innerHTML = quizPage;
 }
